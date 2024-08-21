3e99917f72ab4765fe4f6cdfd09d384afdbe5c1b
Felipe Basini <88778207+fbasini@users.noreply.github.com>
2024-08-21 16:41:09 -0300

    Without inheritance

--- ContosoUniversity/ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51a2137354fa1127991a954c77f41a06aebe6c44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16dd59f143873f57930dbed59542f37a53ef0411")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
